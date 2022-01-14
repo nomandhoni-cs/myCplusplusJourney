7df1d7e2b995728bd11a428cc2eaaf63ee1d07c4
nomandhoni-cs <nomandhoni@yahoo.com>
2022-01-14 18:37:54 +0600

    Added Data Types

---
+++ data-types.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+//Here we use "int" for Intiger Variable.
+	int a;
+	a=10;
+
+//Here we use "float/double" for float Variable.
+    float b;
+    b=33.33;
+	double c;
+	c=44.44;
+//Short Variable need most small amount of storage.
+    short d;
+    d=50;
+//long Variable need big amount of storage.
+    long e;
+    e=60;
+//long long Variable need most big amount of storage.
+    long long f;
+    f=70;
+//You can you long in double variable
+    long double g;
+    g=88.88;
+//Output Section Here we use "cout"	to print.
+	cout<<"The Intiger is="<<a<<endl;
+	cout<<"The float is="<<b<<endl;
+	cout<<"The double is="<<c<<endl;
+	cout<<"The short is="<<d<<endl;
+	cout<<"The long is="<<e<<endl;
+	cout<<"The long long is="<<f<<endl;
+	cout<<"The long double is="<<g<<endl;
+	return 0;
+	}
