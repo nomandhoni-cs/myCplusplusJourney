8c614b8fcf638a8d027d95b3629da177a7691fcb
nomandhoni-cs <nomandhoni@yahoo.com>
2022-01-31 17:08:16 +0600

    Added Comments

--- 1-dimention-basic-array.cpp
+++ 1-dimention-basic-array.cpp
@@ -2,21 +2,33 @@
 using namespace std;
 int main()
 {
-	int a;
-/*	cout<<"How much long is your array ";
-	cin>>a; */
-	int myarr[6];
-for(int i=0; i<6; i++)
-	{
-		cout<<"Enter the marks ";
-		cin>>myarr[a];
-	}
+	int a=0, k, b=0, fullMarks, m;
+	cout<<"How much student is in your class ";
+	cin>>k;
+	cout<<"Full marks of your Exam ";
+	cin>>fullMarks;
+	int marks[k];
+	string name[k];
 	
-for(int j=0; j<4; j++)
+for(int i=0; i<k; i++)
 	{
-		cout<<"The value "<<myarr[a]<<endl;
-		a=a-1;
+		cout<<"Enter the Name ";
+		cin>>name[b];
+		cout<<"Enter the marks ";
+		cin>>marks[a];		
+		b=b+1;
+		a=a+1;
 	}
-
+a=0;
+b=0;
+	//Used this loop to output all the values of that array & added 2 array one for store name and one for store marks.
+for(int j=0; j<k; j++)
+	{
+		marks[a] = m;
+		cout<<name[b]<<" got "<<marks[a]<<" marks = "<<((m)/100)*fullMarks<< "%"<<endl;
+		a++;
+		b++;
+		}
 	return 0;
 }
+	
