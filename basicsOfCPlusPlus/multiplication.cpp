9248e5a660194c6f759ab3aa29eb0a0a6f1052b4
nomandhoni-cs <nomandhoni@yahoo.com>
2022-10-14 21:06:45 +0600

    Learned `sizeof()` opereator

--- basicsOfCPlusPlus/multiplication.cpp
+++ basicsOfCPlusPlus/multiplication.cpp
@@ -7,6 +7,7 @@ int main(){
     cout << "Enter b " << endl;
     cin >> b;
     multiplication = (a * b);
-    cout << "The Multipliccation is " << multiplication;
+    cout << "The Multipliccation is " << multiplication << endl;
+    cout << sizeof(multiplication);
     return 0;
 }
